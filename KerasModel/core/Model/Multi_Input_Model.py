24ab7877ef8e147e5dd954b88e4103074d25fe50
gaxu <48383026+gaxu@users.noreply.github.com>
2021-01-28 12:01:17 +0800

    Update Multi_Input_Model.py

--- KerasModel/core/Model/Multi_Input_Model.py
+++ KerasModel/core/Model/Multi_Input_Model.py
@@ -39,12 +39,14 @@ def multi_input_model(img_height, img_width, img_channl,num_classes):
     x1=Activation(mish)(x1)
     x1=MaxPooling2D((2,2),strides=(2,2))(x1)
     x1=Conv2D(32,(5,5),strides=(1,1),padding='valid',name='conv2')(x1)
+    x1=MaxPooling2D((2,2),strides=(2,2))(x1)
  
-    x2 = Conv2D(32,(5,5),strides=(1,1),padding='valid',name='conv3')(input2_)
+    x2=Conv2D(32,(5,5),strides=(1,1),padding='valid',name='conv3')(input2_)
     x2=Activation(mish)(x2)
     x2=MaxPooling2D((2,2),strides=(2,2))(x2)
     x2=Conv2D(32,(5,5),strides=(1,1),padding='valid',name='conv4')(x2)
-
+    x2=MaxPooling2D((2,2),strides=(2,2))(x2)
+    
     x = Add()([x1, x2])
     x = Flatten()(x)
 
